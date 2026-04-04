9dfb7f3e99a856641617c63fcb2e1d1684fb4f7c
arwaAWWAD-D <s12442120@stu.najah.edu>
2026-04-04 14:42:41 +0300

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Exams from "./pages/Exams";
+import LoginModal from "./components/LoginModal";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showLogin, setShowLogin] = useState(true);
+
   return (
     <BrowserRouter>
-      <Navbar />
+      {!isLoggedIn && showLogin && (
+        <LoginModal
+          isOpen={showLogin}
+          onClose={() => setShowLogin(false)}
+          onLoginSuccess={() => {
+            setIsLoggedIn(true);
+            setShowLogin(false);
+          }}
+        />
+      )}
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      {isLoggedIn && <Navbar />}
+
+      {isLoggedIn && (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exams" element={<Exams />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      )}
+
+      {!isLoggedIn && !showLogin && (
+        <div className="flex justify-center mt-10">
+          <button
+            onClick={() => setShowLogin(true)}
+            className="bg-blue-600 text-white p-2 rounded"
+          >
+            Login
+          </button>
+        </div>
+      )}
     </BrowserRouter>
   );
 }

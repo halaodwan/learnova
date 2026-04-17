4ab280ddb9fc842b711adc37a15fa345c31f3a34
mai <s12400416@stu.najah.edu>
2026-04-17 18:23:22 +0300

    update content or questions

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ const flashcardRoutes = require('./Routes/FlashcardRoutes');
 
 const examRoutes = require('./Routes/ExamRoutes');
 
-const questionRoutes = require('./Routes/QuestionRoutes');
+const QuestionRoutes = require('./Routes/QuestionRoutes');
 
 const optionRoutes = require('./Routes/OptionRoutes');
 
@@ -40,7 +40,7 @@ app.use('/flashcards', flashcardRoutes);
 
 app.use('/exams', examRoutes);
 
-app.use('/questions', questionRoutes);
+app.use('/questions', QuestionRoutes);
 
 app.use('/options', optionRoutes);
 
